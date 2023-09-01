d5ebf463a46c85c63f5a37616d0e9b164fb65c01
Egor O'Sten <borodark@gmail.com>
2023-08-31 23:12:00 -0400

    tests docs

--- Readme.md
+++ Readme.md
@@ -1,3 +1,51 @@
+# Integer Varaible Lengh Encoding Decoding
+
+
+The solution uses as little memory as possible dynamically adding space to the buffer as needed
+Reliably package only significant bytes of integers, uses 'i' as separator followed directly by the number of bytes occupied by the value of integer. Next are significant bytes of an integer value encoded:
+
+
+```python3
+
+[-1, 1, 9223372036854775807, 65536, -109870879, -9223372036854775807]
+```
+
+encodes into:
+
+```python3
+b'V1[i\x01\xffi\x01\x01i\x08\x7f\xff\xff\xff\xff\xff\xff\xffi\x03\x01\x00\x00i\x04\xf9s\x80\xe1i\x08\x80\x00\x00\x00\x00\x00\x00\x01'
+ ```
+
+- Inspired by https://medium.com/@niamtokik/serialization-series-do-you-speak-erlang-etf-or-bert-part-1-ff70096b50c0
+
+- Compiled from ideas described here: https://www.erlang.org/doc/man/ei
+
+Install deps:
+
+`pip3 install -r deps`
+
+Start python interpreter and import the `app`:
+
+```
+io@Igors-MacBook:~/projects/ch-code-test$ python3
+Python 3.11.5 (main, Aug 24 2023, 15:23:14) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import app
+[-1, 1, 9223372036854775807, 65536, -109870879, -9223372036854775807]
+'Memory size of list:'
+312
+'Memory size of encoded buffer '
+80
+[-1, 1, 9223372036854775807, 65536, -109870879, -9223372036854775807]
+'are lists the same?'
+True
+>>> 
+```
+
+
+
+## WIP
+
 type Data = Array<string | int32 | Data>
 
 Yes, it's OR
@@ -9,27 +57,6 @@ both are valid inputs
 
 [11, foo, [foo, 11], [in, [ deeper, [ even_deeper , 9999 ]]]].
 
-```
-
 [boo, 11, [bar ,32], [fa,233],]
 
-Inspired by https://medium.com/@niamtokik/serialization-series-do-you-speak-erlang-etf-or-bert-part-1-ff70096b50c0
-
-Compiled from ideas described here: https://www.erlang.org/doc/man/ei
-
-
-```
->>> b =b'\xff'
->>> int.from_bytes(b, 'big', signed=True)
--1
->>> b =b'\x01\x00\x00'
->>> int.from_bytes(b, 'big', signed=True)
-65536
->>> b =b'\xf9s\x80\xe1'
->>> int.from_bytes(b, 'big', signed=True)
--109870879
->>> b =b'\x80\x00\x00\x00\x00\x00\x00\x01'
->>> int.from_bytes(b, 'big', signed=True)
--9223372036854775807
->>> 
 ```
--- app.py
+++ app.py
@@ -1,14 +1,15 @@
+from pympler.asizeof import asizeof
 import zlib
 import sys
 from math import log
+
 from pprint import pprint
 
 MAX_STRING_LENGTH=1000000
 
 MAX_ARRAY_SIZE=1000
 
-
-from math import log
+from pympler.asizeof import asizeof
 
 def byte_length(i):
     if i == 0:
@@ -16,77 +17,103 @@ def byte_length(i):
     return (i.bit_length() + 7) // 8
 
 def encode_int32(stream: bytearray, value: int, value_pointer=3):
-    pprint(value)
-    pprint(value_pointer)
+    #pprint(value)
+    #pprint(value_pointer)
     if value > sys.maxsize:
         raise Exception('value > sys.maxsize') 
     int_length_in_bytes_= byte_length(value)
-    pprint(int_length_in_bytes_)
+    #pprint(int_length_in_bytes_)
     stream.extend(bytes(2 + int_length_in_bytes_ ))
-    pprint(stream)
+    #pprint(stream)
     stream_raw= memoryview(stream)
     stream_raw[value_pointer:value_pointer+1]= b'i'
     value_pointer= value_pointer + 1
-    pprint(stream)
+    #pprint(stream)
     stream_raw[value_pointer:value_pointer+1]= int_length_in_bytes_.to_bytes()
     # should be one byte for int32 length in bytes
-    pprint(stream)
+    #pprint(stream)
     value_pointer= value_pointer + 1
     stream_raw[value_pointer:value_pointer + int_length_in_bytes_]= value.to_bytes(int_length_in_bytes_, signed=True)
     value_pointer= value_pointer + int_length_in_bytes_
     return stream, value_pointer # return pointer to next byte after the value bytes
 
 def decode_int32(stream_bytes: bytearray, i_marker_pointer=3):
-    pprint(stream_bytes)
+    #pprint(stream_bytes)
     stream_raw= memoryview(stream_bytes)
     length_in_bytes= stream_raw[i_marker_pointer+1:i_marker_pointer+2]
     of_bytes_of_length= int.from_bytes(length_in_bytes, 'big')
-    pprint(of_bytes_of_length)
+    #pprint(of_bytes_of_length)
     value_in_bytes= stream_raw[i_marker_pointer+2:i_marker_pointer+2+of_bytes_of_length]
-    pprint(value_in_bytes.tobytes())
-    return int.from_bytes(value_in_bytes, 'big', signed=True)
+    #pprint(value_in_bytes.tobytes())
+    return int.from_bytes(value_in_bytes, 'big', signed=True), i_marker_pointer+2+of_bytes_of_length # pointer to next marker
+
 
 
 buf_ = bytearray(b'V1[')
-buf_, pointer_to_the_end_of_stream= encode_int32(buf_,-1)
-pprint('stream')
-pprint(buf_)
-pprint(pointer_to_the_end_of_stream)
-buf_, pointer_to_the_end_of_stream= encode_int32(buf_,1,value_pointer=pointer_to_the_end_of_stream)
-pprint(pointer_to_the_end_of_stream)
-pprint('stream')
-pprint(buf_)
-buf_, pointer_to_the_end_of_stream= encode_int32(buf_,sys.maxsize,value_pointer=pointer_to_the_end_of_stream)
-pprint(pointer_to_the_end_of_stream)
-pprint('stream')
-pprint(buf_)
-buf_, pointer_to_the_end_of_stream= encode_int32(buf_,65536,value_pointer=pointer_to_the_end_of_stream)
-pprint(pointer_to_the_end_of_stream)
-pprint('stream')
-pprint(buf_)
-buf_, pointer_to_the_end_of_stream= encode_int32(buf_, -109870879, value_pointer=pointer_to_the_end_of_stream)
-pprint(pointer_to_the_end_of_stream)
-pprint('stream')
-pprint(buf_)
-buf_, pointer_to_the_end_of_stream= encode_int32(buf_,-sys.maxsize, value_pointer=pointer_to_the_end_of_stream)
-pprint(pointer_to_the_end_of_stream)
-pprint('stream')
-pprint(buf_)
+integers= [-1,1,sys.maxsize,65536,-109870879,-sys.maxsize]
+pprint(integers)
+pprint("Memory size of list:")
+pprint(asizeof(integers))
+pointer_to_the_end_of_stream=3
+for an_int32 in integers:
+    buf_, pointer_to_the_end_of_stream= encode_int32(buf_,an_int32,value_pointer=pointer_to_the_end_of_stream)
+    #pprint('stream')
+    #pprint(buf_)
+    #pprint(pointer_to_the_end_of_stream)
+#buf_, pointer_to_the_end_of_stream= encode_int32(buf_,1,value_pointer=pointer_to_the_end_of_stream)
+#pprint(pointer_to_the_end_of_stream)
+#pprint('stream')
+#pprint(buf_)
+#buf_, pointer_to_the_end_of_stream= encode_int32(buf_,sys.maxsize,value_pointer=pointer_to_the_end_of_stream)
+#pprint(pointer_to_the_end_of_stream)
+#pprint('stream')
+#pprint(buf_)
+#buf_, pointer_to_the_end_of_stream= encode_int32(buf_,65536,value_pointer=pointer_to_the_end_of_stream)
+#pprint(pointer_to_the_end_of_stream)
+#pprint('stream')
+#pprint(buf_)
+#buf_, pointer_to_the_end_of_stream= encode_int32(buf_, -109870879, value_pointer=pointer_to_the_end_of_stream)
+#pprint(pointer_to_the_end_of_stream)
+#pprint('stream')
+#pprint(buf_)
+#buf_, pointer_to_the_end_of_stream= encode_int32(buf_,-sys.maxsize, value_pointer=pointer_to_the_end_of_stream)
+#pprint(pointer_to_the_end_of_stream)
+#pprint('stream final..')
 
+m = memoryview(buf_)
+b= m.tobytes()
+pprint("Memory size of encoded buffer ")
+pprint(asizeof(b))
+pprint(b)
 
-b= b'V1[i\x08\x80\x00\x00\x00\x00\x00\x00\x01i\x01\xff'
-i= decode_int32(b, i_marker_pointer=3)
-pprint(i)
-i= decode_int32(b, i_marker_pointer=13)
-pprint(i)
+unpacked_int23z= []
+an_int32, next_marker_pointer= decode_int32(b, i_marker_pointer=3)
+unpacked_int23z.append(an_int32)
+#pprint({"an_int32":an_int32})
+#pprint({"next_marker_pointer":next_marker_pointer})
+an_int32, next_marker_pointer= decode_int32(b, i_marker_pointer=next_marker_pointer)
+unpacked_int23z.append(an_int32)
+#pprint({"an_int32":an_int32})
+#pprint({"next_marker_pointer":next_marker_pointer})
+an_int32, next_marker_pointer= decode_int32(b, i_marker_pointer=next_marker_pointer)
+unpacked_int23z.append(an_int32)
+#pprint({"an_int32":an_int32})
+#pprint({"next_marker_pointer":next_marker_pointer})
+an_int32, next_marker_pointer= decode_int32(b, i_marker_pointer=next_marker_pointer)
+unpacked_int23z.append(an_int32)
+#pprint({"an_int32":an_int32})
+#pprint({"next_marker_pointer":next_marker_pointer})
+an_int32, next_marker_pointer= decode_int32(b, i_marker_pointer=next_marker_pointer)
+unpacked_int23z.append(an_int32)
+#pprint({"an_int32":an_int32})
+#pprint({"next_marker_pointer":next_marker_pointer})
+an_int32, next_marker_pointer= decode_int32(b, i_marker_pointer=next_marker_pointer)
+unpacked_int23z.append(an_int32)
+#pprint({"an_int32":an_int32})
+#pprint({"next_marker_pointer":next_marker_pointer})
+pprint(unpacked_int23z)
 
-b= b'V1[i\x01\xffi\x01\x01i\x08\x7f\xff\xff\xff\xff\xff\xff\xffi\x03\x01\x00\x00i\x04\xf9s\x80\xe1i\x08\x80\x00\x00\x00\x00\x00\x00\x01'
-
-i= decode_int32(b, i_marker_pointer=3)
-pprint(i)
-i= decode_int32(b, i_marker_pointer=6)
-pprint(i)
-i= decode_int32(b, i_marker_pointer=9)
-pprint(i)
-i= decode_int32(b, i_marker_pointer=19)
-pprint(i)
+same= integers == unpacked_int23z
+pprint("are lists the same?")
+pprint(same)
+#b= b'V1[i\x01\xffi\x01\x01i\x08\x7f\xff\xff\xff\xff\xff\xff\xffi\x03\x01\x00\x00i\x04\xf9s\x80\xe1i\x08\x80\x00\x00\x00\x00\x00\x00\x01'
