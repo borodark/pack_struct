4a7eb410cf4fdc412645b144c357eb81fa585f11
Egor O'Sten <borodark@gmail.com>
2023-08-31 20:16:55 -0400

    no separators

--- app.py
+++ app.py
@@ -20,39 +20,33 @@ def encode_int32(stream: bytearray, value: int, value_pointer=3):
     if value > sys.maxsize:
         raise Exception('value > sys.maxsize') 
     int_length_in_bytes_= byte_length(value)
-    pprint(int_length_in_bytes_)
+    #pprint(int_length_in_bytes_)
     # bytes_of_lenth =int_length_in_bytes_.to_bytes(signed=False)
     # append bytes of marker of int in the form of "i=<int_lenth_in_bytes_>|<bytes of value>|"
-    stream.extend(bytes(5 + int_length_in_bytes_ ))
+    stream.extend(bytes(2 + int_length_in_bytes_ ))
     stream_raw= memoryview(stream)
     # value_pointer value: int,
-    stream_raw[value_pointer:value_pointer+2]= b'i='
-    value_pointer= value_pointer + 2
-    pprint(stream)
+    stream_raw[value_pointer:value_pointer+1]= b'i'
+    value_pointer= value_pointer + 1
+    #pprint(stream)
     # stream.append(int_length_in_bytes_)
     stream_raw[value_pointer:value_pointer+1]= int_length_in_bytes_.to_bytes()
     # should be one byte for int32 length in bytes
     value_pointer= value_pointer + 1
-    pprint(stream)
-    stream_raw[value_pointer:value_pointer+1]= b'|'
+    #pprint(stream)
+    #stream_raw[value_pointer:value_pointer+1]= b'|'
     # should be one byte for int32 length in bytes
-    value_pointer= value_pointer + 1
-    pprint(stream)
+    #value_pointer= value_pointer + 1
+    #pprint(stream)
     #pprint(int_length_in_bytes_)
     stream_raw[value_pointer:value_pointer + int_length_in_bytes_]= value.to_bytes(int_length_in_bytes_, signed=True)
-    pprint(stream)
-    value_pointer= value_pointer + int_length_in_bytes_
-    stream_raw[value_pointer:value_pointer + 1]= b'|'
-    # should be one byte for int32 length in bytes
-    value_pointer= value_pointer + 1
-
-    pprint(stream)
-
-    #stream.append(bytes_of_lenth)
-    #stream.append(b'|')
-    #stream.append(int.to_bytes(int_length_in_bytes_, signed=True))
-    #stream.append(b'|')
     #pprint(stream)
+    value_pointer= value_pointer + int_length_in_bytes_
+    #stream_raw[value_pointer:value_pointer + 1]= b'|'
+    # should be one byte for int32 length in bytes
+    #value_pointer= value_pointer + 1
+    pprint('packed value')
+    pprint(stream)
     return stream
 
 
